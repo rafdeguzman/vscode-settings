7469b598a05bcc8b6617b814a26490786ba9a4b4
Rafael Vincent <rafdeguzman1@gmail.com>
2024-08-07 20:10:48 -0400

    added extensions script

---
+++ extensions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+code --install-extension aaron-bond.better-comments
+code --install-extension adpyke.codesnap
+code --install-extension alefragnani.bookmarks
+code --install-extension asvetliakov.vscode-neovim
+code --install-extension christian-kohler.path-intellisense
+code --install-extension drcika.apc-extension
+code --install-extension entuent.fira-code-nerd-font
+code --install-extension esbenp.prettier-vscode
+code --install-extension gruntfuggly.todo-tree
+code --install-extension jonathanharty.gruvbox-material-icon-theme
+code --install-extension sainnhe.gruvbox-material
+code --install-extension usernamehw.errorlens
+code --install-extension vspacecode.whichkey
