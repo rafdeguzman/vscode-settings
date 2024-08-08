1b79929c182c41fcb78168d1cbd65f362394303a
Rafael Vincent <rafdeguzman1@gmail.com>
2024-08-07 20:16:50 -0400

    added parameter

--- extensions.sh
+++ extensions.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
-code --install-extension aaron-bond.better-comments
-code --install-extension adpyke.codesnap
-code --install-extension alefragnani.bookmarks
-code --install-extension asvetliakov.vscode-neovim
-code --install-extension christian-kohler.path-intellisense
-code --install-extension drcika.apc-extension
-code --install-extension entuent.fira-code-nerd-font
-code --install-extension esbenp.prettier-vscode
-code --install-extension gruntfuggly.todo-tree
-code --install-extension jonathanharty.gruvbox-material-icon-theme
-code --install-extension sainnhe.gruvbox-material
-code --install-extension usernamehw.errorlens
-code --install-extension vspacecode.whichkey
+
+# Default command to 'code'
+CMD="code"
+
+# Check if a parameter is provided and use it instead of 'code'
+if [ "$1" ]; then
+  CMD="$1"
+fi
+
+$CMD --install-extension aaron-bond.better-comments
+$CMD --install-extension adpyke.codesnap
+$CMD --install-extension alefragnani.bookmarks
+$CMD --install-extension asvetliakov.vscode-neovim
+$CMD --install-extension christian-kohler.path-intellisense
+$CMD --install-extension drcika.apc-extension
+$CMD --install-extension entuent.fira-code-nerd-font
+$CMD --install-extension esbenp.prettier-vscode
+$CMD --install-extension gruntfuggly.todo-tree
+$CMD --install-extension jonathanharty.gruvbox-material-icon-theme
+$CMD --install-extension sainnhe.gruvbox-material
+$CMD --install-extension usernamehw.errorlens
+$CMD --install-extension vspacecode.whichkey
